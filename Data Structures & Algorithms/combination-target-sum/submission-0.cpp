53ec7f9ff826c37fda516ae6c94102bf7b24a27c
bisesh333-prog <ajdynamo333@gmail.com>
2026-04-28 01:30:55 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        int i=0;
+        vector<int> tmp;
+        backtrack(nums, i, tmp, target);
+        return ans;
+    }
+    void backtrack(vector<int>& nums, int i, vector<int> &tmp, int target){
+        if(target==0){
+            ans.push_back(tmp);
+            return;
+        }
+        if(i==nums.size()||target<0) return;
+
+        tmp.push_back(nums[i]);
+        backtrack(nums,i,tmp, target-nums[i]);
+        tmp.pop_back();
+        backtrack(nums,i+1,tmp,target);
+    }
+};
